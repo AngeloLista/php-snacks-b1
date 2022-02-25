b1d84ea58f4ba72e450f88c300f7ee5fff9b4352
AngeloLista <abstractplayer@gmail.com>
2022-02-25 15:20:17 +0100

    added validation

--- snack1/index.php
+++ snack1/index.php
@@ -10,7 +10,7 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snack 1</title>
 </head>
 
 <?php
@@ -21,9 +21,49 @@ $age = $_GET['age'] ?? '';
 
 <body>
     <h1>Snack 1</h1>
-    <p>Name: <?= $name ?></p>
-    <p>E-mail: <?= $email ?></p>
-    <p>Age: <?= $age ?></p>
+    <p>Name:
+        <?php
+        // Name length validation
+        if (strlen($name) > 3) {
+            echo $name;
+        } else {
+            echo '!!!ATTENZIONE!!! Il nome deve essere più lungo di 3 caratteri';
+        } ?>
+    </p>
+    <p>E-mail:
+        <?php
+        if (empty($email)) {
+            echo 'Nessuna E-Mail ricevuta';
+        } else {
+            // E-mail characters validation
+            if (strpos($email, '@') && strpos($email, '.')) {
+                echo $email;
+            } else {
+                echo "L'E-mail inserita non è valida. Deve contenere un '.' e '@'.";
+            }
+        }
+        ?>
+    </p>
+    <p>Age:
+        <?php
+        // Value type validation
+        if (is_numeric($age)) {
+            echo $age;
+        } else {
+            echo 'Il numero inserito non è valido.';
+        }
+        ?>
+    </p>
+
+    <p>
+        <?php
+        if ((strlen($name) > 3) && (strpos($email, '@') && strpos($email, '.')) && is_numeric($age)) {
+            echo 'ACCESSO RIUSCITO';
+        } else {
+            echo 'ACCESSO NEGATO';
+        }
+        ?>
+    </p>
 
 </body>
 
